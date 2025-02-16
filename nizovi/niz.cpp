91b8152adc8b1073530d0261be28c11d9173b9db
Milos Milivojevic <tvoj-email@gmail.com>
2025-02-16 22:43:01 +0100

    dodat primer nizova

---
+++ nizovi/niz.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+
+int main() {
+
+    int niz[]{1,2,3,4,5,6};
+
+    std::cout << "ispis niza\n";
+
+    for(int i = 0;i<6;i++) {
+
+        std::cout << niz[i] << " ";
+    }
+
+}
